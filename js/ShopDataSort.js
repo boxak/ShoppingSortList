debccb2d746c7477b64d370b24b855dc1a2fa8dd
boxak <boxak@naver.com>
2021-01-29 02:54:35 +0900

    draggable sort 작업중

--- js/ShopDataSort.js
+++ js/ShopDataSort.js
@@ -1,14 +1,62 @@
-function print() {
-    var url = document.getElementById("input").value;
+var inx = 0;
+
+function addItem() {
+    var url = document.getElementById("itemInput").value;
 
     var request = new XMLHttpRequest();
     request.onload = function() {
         if (request.status == 200) {
             var str = request.responseText;
             var obj = JSON.parse(str);
-            console.log(obj);
+            makeItemHtml(obj);
         }
     }
     request.open("GET","http://localhost:8080/parsing?url="+url,true);
     request.send();
 }
+
+function makeItemHtml(item) {
+    var mainDom = document.getElementById("main-page");
+    var innerElem = mainDom.innerHTML;
+    mainDom.innerHTML = "";
+
+    var appendedElem = appendElement(item);
+    mainDom.innerHTML+=appendedElem;
+    mainDom.innerHTML+=innerElem;
+}
+
+function appendElement(item) {
+    var str = "";
+    var itemId = "itemId" + inx;
+
+    str+="<div class='itemClass' id='"+ itemId +"' draggable='true'>"
+    str+="<img src='" + item.imgUrl + "'>";
+    str+="<p>"+ item.shopName +"</p>";
+    str+="<p>"+ item.lowestPrice + "</p>";
+    str+="<p>"+ item.starRate + "</p>";
+    str+="</div>"
+    str+="<br>"
+    
+    inx++;
+
+    return str;
+}
+
+const draggables = document.querySelectorAll(".itemClass");
+const container = document.querySelectorAll("#main-page");
+
+draggables.forEach(draggable => {
+    draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+    });
+
+    draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+    });
+});
+
+container.addEventListener('dragover', e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+})
